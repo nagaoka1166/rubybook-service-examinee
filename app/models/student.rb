2db333a2b8c92e0e324588a3929f0f8352aa0f6f
nagaoka1166 <Ryuu11660408@gmail.com>
2021-12-10 15:28:54 +0900

    ログイン画面と垢登録画面フロント実装

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,22 @@
 class Student < ApplicationRecord
     belongs_to :user
-    # validates :grade, presence: true
-    validates :grade, absence: true, if: :content_type == 2
-    validates :grade, presence: true, if: :content_type == 1
+validates_associated :user
+validate :grade_validate_researcher
+validate :grade_validate_student
+
+
+def grade_validate_researcher
+  if self.user.content_type == 2 && grade.present?
+    errors.add(:grade, 'を記入しないでください')
+  end
+end
+
+def grade_validate_student
+if self.user.content_type == 1 && grade.nil?
+    errors.add(:grade, 'ここに文章を入れます')
+  end
+end
+
     enum grade:{
        zero: 0, one:1,two:2,three:3,four:4,five:5,six:6,
             M1:7,M2:8,M3:9,M4:10
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
+
   def liked_by?(post_id)
     likes.where(post_id: post_id).exists?
   end
