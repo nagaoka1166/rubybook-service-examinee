3d0c9cdd5b0068b029e83f86427e81b770d96116
nagaoka1166 <Ryuu11660408@gmail.com>
2022-10-27 19:17:50 +0900

    mailer-awsの設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,17 +35,17 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.default_url_options = { host: "http://tesma.jp"}
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :aws_sdk
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    enable_starttls_auto: true,
-    port:                 587,
-    domain:             '@gmail.com',
-    user_name:        'ryuu11660408@gmail.com',
-    password:             'アプリパスワード',
-    authentication:       :plain
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   enable_starttls_auto: true,
+  #   port:                 587,
+  #   domain:             '@gmail.com',
+  #   user_name:        'ryuu11660408@gmail.com',
+  #   password:             'アプリパスワード',
+  #   authentication:       :plain
+  # }
 
   config.action_mailer.perform_caching = false
 
